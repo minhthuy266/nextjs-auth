0670cffdc163bf15e411e92eebe70fa86ac2155e
thuynm <thuynm@visionnetwork.vn>
2022-10-12 17:35:25 +0700

    Server side authentication

--- pages/blog.js
+++ pages/blog.js
@@ -1,5 +1,21 @@
-function Blog() {
-  return <h1>Blog page</h1>;
+import { getSession } from "next-auth/react";
+
+function Blog({ blogsdata }) {
+  return <h1>Blog Page - {blogsdata}</h1>;
 }
 
 export default Blog;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  console.log(session);
+
+  return {
+    props: {
+      blogsdata: session
+        ? "List of 100 personalizedblogs"
+        : "List of free blogs",
+    },
+  };
+}
