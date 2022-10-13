bf30ef949bd50d53a0881c9a680dd4eeb95f1fc1
thuynm <thuynm@visionnetwork.vn>
2022-10-13 09:36:54 +0700

    Connecting to a Database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import clientPromise from "./lib/mongodb";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,8 +10,20 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+
     // ...add more providers here
   ],
+
+  database: process.env.DB_URL,
+  session: {
+    jwt: true,
+  },
+
+  adapter: MongoDBAdapter(clientPromise),
+
+  jwt: {
+    secret: "thuynm266",
+  },
 };
 
 export default NextAuth(authOptions);
